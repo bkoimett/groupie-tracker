dcca4336a6c633932ce2b3a567a704a0b82c053f
bkoimett <benkoimet@gmail.com>
2026-03-02 14:12:33 +0300

    refactor: add more models

--- models/models.go
+++ models/models.go
@@ -8,6 +8,40 @@ type Artist struct {
     Members      []string `json:"members"`
     CreationDate int      `json:"creationDate"`
     FirstAlbum   string   `json:"firstAlbum"`
+    // We'll add these fields to store related data
+    Locations    []string `json:"-"`
+    ConcertDates []string `json:"-"`
+    Relations    map[string][]string `json:"-"`
 }
 
-// We'll add Locations, Dates, and Relation structs later
+// LocationsAPI represents the response from /locations/{id}
+type LocationsAPI struct {
+    Index []struct {
+        ID        int      `json:"id"`
+        Locations []string `json:"locations"`
+    } `json:"index"`
+}
+
+// DatesAPI represents the response from /dates/{id}
+type DatesAPI struct {
+    Index []struct {
+        ID    int      `json:"id"`
+        Dates []string `json:"dates"`
+    } `json:"index"`
+}
+
+// RelationAPI represents the response from /relation/{id}
+type RelationAPI struct {
+    Index []struct {
+        ID             int                 `json:"id"`
+        DatesLocations map[string][]string `json:"datesLocations"`
+    } `json:"index"`
+}
+
+// FullArtistData combines all information for display
+type FullArtistData struct {
+    Artist
+    Locations    []string
+    Dates        []string
+    Relations    map[string][]string
+}
