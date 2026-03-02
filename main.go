d67bde2a0d988e95eaba8d2c1b51c6b02fa7f7d0
bkoimett <benkoimet@gmail.com>
2026-03-02 14:14:03 +0300

    feat: add artist handler to get artist details with locations and dates. Also add cache for the templates for reuse

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
     "fmt"
+    "html/template"
     "log"
     "net/http"
     "groupie-tracker/handlers"
 )
+
+// TemplateCache stores parsed templates for reuse
+var TemplateCache *template.Template
+
+func init() {
+    // Register template functions
+    funcMap := template.FuncMap{
+        "formatLocation": handlers.FormatLocation,
+        "formatDate":     handlers.FormatDate,
+    }
+
+    // Parse all templates with functions
+    TemplateCache = template.Must(template.New("").Funcs(funcMap).ParseGlob("templates/*.html"))
+    
+    // Make templates available to handlers
+    handlers.SetTemplateCache(TemplateCache)
+}
 
 func main() {
     // Serve static files
@@ -14,11 +32,13 @@ func main() {
 
     // Register handlers
     http.HandleFunc("/", handlers.HomeHandler)
+    http.HandleFunc("/artist", handlers.ArtistHandler)
 
     // Start server
     port := ":8080"
     fmt.Printf("🚀 Server starting on http://localhost%s\n", port)
     fmt.Println("📦 Press Ctrl+C to stop")
+    fmt.Println("✨ New today: Artist details pages with locations and dates!")
     
     err := http.ListenAndServe(port, nil)
     if err != nil {
