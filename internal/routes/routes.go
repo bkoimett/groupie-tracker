82c672b5d2d57e9de42973ca178b5ddb97099612
bkoimett <benkoimet@gmail.com>
2026-03-18 10:13:37 +0000

    fix: solve issue with css display when rendering

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,25 +1,43 @@
 package routes
 
 import (
-	"net/http"
+    // "groupie-tracker/internal/handlers"
+    "net/http"
+    "path/filepath"
+    "strings"
 )
 
 type Routes struct {
-	Method  string
-	Path    string
-	Handler http.HandlerFunc
+    Method  string
+    Path    string
+    Handler http.HandlerFunc
 }
 
-func NewRoutes(routes []Routes) http.Handler {
-	mux := http.NewServeMux()
-	// Serve static files
-	fs := http.FileServer(http.Dir("../internal/static"))
-	mux.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	// Register handlers
-	for _, i := range routes {
-		mux.HandleFunc(i.Path, i.Handler)
-	}
-
-	return mux
+func NewRoutes(routeList []Routes) http.Handler {
+    mux := http.NewServeMux()
+    
+    // Custom static file handler
+    fs := http.FileServer(http.Dir("internal/static"))
+    mux.Handle("/static/", http.StripPrefix("/static/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        // Set correct MIME types
+        ext := strings.ToLower(filepath.Ext(r.URL.Path))
+        switch ext {
+        case ".css":
+            w.Header().Set("Content-Type", "text/css; charset=utf-8")
+        case ".js":
+            w.Header().Set("Content-Type", "application/javascript; charset=utf-8")
+        }
+        
+        // Remove nosniff header or keep it but with correct types
+        // w.Header().Del("X-Content-Type-Options")
+        
+        fs.ServeHTTP(w, r)
+    })))
+    
+    // Register all routes
+    for _, route := range routeList {
+        mux.HandleFunc(route.Path, route.Handler)
+    }
+    
+    return mux
 }
